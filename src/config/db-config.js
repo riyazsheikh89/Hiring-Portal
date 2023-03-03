76e782f10fd0071c82b769eaf99c805e76bc3d61
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-03-03 17:18:52 +0530

    updated to mongoDB URI

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connect = async () => {
-    mongoose.connect('mongodb://localhost/Hiring-Portal');
+    mongoose.connect('mongodb+srv://riyaz-atlas:mD7XpJdKm8Ch0q0K@atlas-cluster.6xi3y9g.mongodb.net/Hiring-Portal');
 }
 
 export default connect;
